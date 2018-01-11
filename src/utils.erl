f7b7561f4c3d3af50212ed6b4f3b5a5569e1cf71
Maciej Znamirowski <maciek@znamirowski.pl>
2018-01-11 15:47:44 +0100

    Created concurrent foreach function and shortened concurrent map function.

--- src/utils.erl
+++ src/utils.erl
@@ -5,12 +5,13 @@
 %% API
 -export([distance/2,
   concurrent_map/2,
+  concurrent_foreach/2,
   random_coords/0,
   random_coords/2,
   random_number/2]).
 
 % private
--export([run_function/3]).
+-export([]).
 
 
 %%%===================================================================
@@ -20,8 +21,18 @@
 distance(#coords{x=X1, y=Y1}, #coords{x=X2, y=Y2}) ->
   math:sqrt(math:pow(X2 - X1, 2) + math:pow(Y2 - Y1, 2)).
 
-concurrent_map(Function, List) ->
-  try run_processes(Function, List) of
+concurrent_map(Function, List) when is_function(Function, 1)
+    andalso is_list(List) ->
+  try map_run_processes(Function, List) of
+    X -> X
+  catch
+    error:Error -> {error, Error};
+    exit:Exit -> {exit, Exit}
+  end.
+
+concurrent_foreach(Function, List) when is_function(Function, 1)
+    andalso is_list(List) ->
+  try run_processes_foreach(Function, List) of
     X -> X
   catch
     error:Error -> {error, Error};
@@ -49,16 +60,12 @@ random_number(Min, Max) ->
 %%% private functions
 %%%===================================================================
 
-run_processes(Function, List) ->
-  register(main_pid, self()),
-  PidList = lists:map(fun(El) -> run_process(self(), El, Function) end, List),
+map_run_processes(Function, List) ->
+  PidList = lists:map(fun(El) -> map_run_process(self(), El, Function) end, List),
   read_mapped_list(PidList, []).
 
-run_process(From, El, Function) ->
-  spawn_link(?MODULE, run_function, [From, El, Function]).
-
-run_function(From, El, Function) ->
-  From ! {self(), Function(El)}.
+map_run_process(From, El, Function) ->
+  spawn_link(fun() -> From ! {self(), Function(El)} end).
 
 read_mapped_list([], Received) ->
   lists:reverse(Received);
@@ -67,3 +74,7 @@ read_mapped_list([Pid|T], Received) ->
   receive
     {Pid, Result} -> read_mapped_list(T, [Result | Received])
   end.
+
+
+run_processes_foreach(Function, List) ->
+  lists:foreach(fun(El) -> spawn_link(fun() -> Function(El) end) end, List).
